14c8f1ed70e713f814ea0a635ed438feac33ae07
abdullah000123 <151346487+abdullah000123@users.noreply.github.com>
2024-12-11 12:44:41 +0500

    Update cnn_using_class_method.py

--- cnn_using_class_method.py
+++ cnn_using_class_method.py
@@ -4,7 +4,7 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 
 print('loading data')
-data = 'data/data'  # Path to the dataset
+data = 'data/train'  # Path to the dataset
 labels = []
 images = []
 image_size = 240
@@ -17,7 +17,7 @@ for imagefile in os.listdir(data):
         print(f"Failed to load image: {imagefile}")
         continue  # Skip if image fails to load
 
-    image = cv.resize(image, (image_size, image_size))  # Resize image to (120, 120)
+    image = cv.resize(image, (image_size, image_size)).astype(np.float16)/255 # Resize image to (120, 120)
 
     # Assign labels based on file name
     if imagefile.startswith("c"):
@@ -49,6 +49,8 @@ labels_one_hot = labels_one_hot[indices]  # Shuffle corresponding one-hot encode
 
 # Print shuffled data details
 print('Sample label (one-hot encoded):', labels_one_hot[0])
+
+
 print('Array images dimension:', images.shape)
 print('Image dimension:', images[0].shape)
 print('Labels one-hot shape:', labels_one_hot.shape)
@@ -60,7 +62,7 @@ stride = 1
 padding = 0
 
 
-batch_sz = 3
+batch_sz = 5
 num_batches = len(images) // batch_sz
 batch_img = []
 batch_lab = []
@@ -72,14 +74,12 @@ for h in range(num_batches):
     batch_l = labels_one_hot[start:end]
     batch_img.append(batch)
     batch_lab.append(batch_l)
-batch_img = np.array(batch_img).astype(np.float64)
+batch_img = np.array(batch_img).astype(np.float32)
 batch_lab = np.array(batch_lab)
 print(batch_img[0])
 print(batch_lab[0])
 plt.imshow(batch_img[10 , 0])
 print('label' ,batch_lab[10 ,0])
-
-
 class Activation:
     def relu_derivative(self ,da ,z):
         return da*(z > 0)
@@ -101,6 +101,63 @@ class Activation:
         return exp_x / (np.sum(exp_x, axis = 1, keepdims = True) + 0.0000005)
         
 ############################################################################################################
+
+class LayerNormalizationSingleBatch:
+    def __init__(self, epsilon=1e-6):
+        self.epsilon = epsilon  # Small constant to avoid division by zero
+
+    def forward(self, x):
+
+        # Compute mean and variance over the (c, h, w) dimensions
+        self.mean = np.mean(x, axis=(0 ,1, 2), keepdims=True)  # Shape: (1, 1, 1)
+        self.variance = np.var(x, axis=(0 ,1, 2), keepdims=True)  # Shape: (1, 1, 1)
+        self.x_normalized = (x - self.mean) / np.sqrt(self.variance + self.epsilon)  # Normalize
+        return self.x_normalized
+
+    def backward(self, dout):
+     
+        c, h, w = dout.shape
+        num_elements = c * h * w  # Total number of elements in (c, h, w)
+
+        # Gradient of normalized input
+        dx_normalized = dout
+
+        # Gradient of variance
+        dvar = np.sum(dx_normalized * (self.x_normalized * -0.5) / np.sqrt(self.variance + self.epsilon), axis=(0 ,1, 2), keepdims=True)
+
+        # Gradient of mean
+        dmean = np.sum(dx_normalized * -1 / np.sqrt(self.variance + self.epsilon), axis=(0 ,1, 2), keepdims=True) \
+                + dvar * np.sum(-2 * (self.x_normalized), axis=(0 ,1, 2), keepdims=True) / num_elements
+
+        # Gradient of input
+        dx = dx_normalized / np.sqrt(self.variance + self.epsilon) \
+             + dvar * 2 * (self.x_normalized) / num_elements \
+             + dmean / num_elements
+
+        return dx
+        
+class LayerNormalization_dense:
+    def __init__(self, epsilon=1e-6):
+        self.epsilon = epsilon  # Small constant to avoid division by zero
+
+    def forward(self, x):
+    
+        self.mean = np.mean(x, axis=-1, keepdims=True)  # Compute mean along the last axis
+        self.variance = np.var(x, axis=-1, keepdims=True)  # Compute variance along the last axis
+        self.x_normalized = (x - self.mean) / np.sqrt(self.variance + self.epsilon)  # Normalize
+        return self.x_normalized
+
+    def backward(self, dout):
+        
+        N = dout.shape[-1]  # Number of features
+        dx_normalized = dout
+        dvar = np.sum(dx_normalized * (self.x_normalized * -0.5) / np.sqrt(self.variance + self.epsilon), axis=-1, keepdims=True)
+        dmean = np.sum(dx_normalized * -1 / np.sqrt(self.variance + self.epsilon), axis=-1, keepdims=True) + \
+                dvar * np.sum(-2 * (self.x_normalized), axis=-1, keepdims=True) / N
+        dx = dx_normalized / np.sqrt(self.variance + self.epsilon) + dvar * 2 * (self.x_normalized) / N + dmean / N
+        return dx
+
+########################################################################################################
 
 class convolution:        
     def conv2(self ,l1_filter ,images ,b ,layer):
@@ -145,7 +202,7 @@ class convolution:
         (pre_map, n_H_prev, n_W_prev) = A_prev.shape
         (no_filters, f, f) = weight.shape #filter of current layer
         (m ,nh ,nw) = dZ.shape       #derivative of loss wrt ouput
-        print('dz and input to this layer in back propogation maps',dZ.shape[0],A_prev.shape[0])
+        #print('dz and input to this layer in back propogation maps',dZ.shape[0],A_prev.shape[0])
         #print('no of filter',weights.shape[0])
         if layer == 1:
             stride = 4 
@@ -172,27 +229,19 @@ class convolution:
                             # gradient of sum (f*a_slide) = sum of gradients (f*dZ)
                         a_slice = A_prev[: ,vert_start: vert_end , horiz_start:horiz_end]
                         a_slice = np.sum(a_slice ,axis=0)
+                        self.dA_prev[: ,vert_start:vert_end , horiz_start:horiz_end] += (weight[c ,: ,:] * dZ[c ,h ,w])    
+                        #self.dA_prev[: ,vert_start:vert_end , horiz_start:horiz_end] = np.sum((weight[c ,: ,:] * dZ[c ,h ,w]))                
 
                     else:
                         a_slice = A_prev[vert_start: vert_end , horiz_start:horiz_end ,:]
                         a_slice = np.sum(a_slice ,axis=2)
                         #dA_prev[vert_start:vert_end, horiz_start:horiz_end,:] += (weight[c,:,:] * dZ[c,h,w]).astype(np.uint8)
-                         
+           
                     self.dW[c ,: ,:] += a_slice * dZ[c ,h ,w]
                     #db +=np.sum(dZ[c,h,w])
-         
-                    self.db[c ,: ,:] += dZ[c, h, w]
+            self.db[c ,: ,:] =np.sum( dZ[c, :, :])
 
-        if layer!=1:
-            for mp in range(no_filters):
-                for h in range(nh):               
-                    for w in range(nw): 
-                        vert_start = h * stride 
-                        vert_end = vert_start + f
-                        horiz_start = w * stride
-                        horiz_end = horiz_start + f
-                        self.dA_prev[: ,vert_start:vert_end , horiz_start:horiz_end] += (weight[mp ,: ,:] * dZ[mp ,h ,w])#.astype(np.float32)
-            print('da prev',self.dA_prev.shape)
+  
                       #db[c,:,:]+=dZ[c,h,w]
         return self.dA_prev, self.dW, self.db
         
@@ -286,14 +335,17 @@ class dense:
     def forward_propagation_dense(self, x):
         #self.a1, self.a2, self.a3 ,self.a4
         z11 = np.dot(x, theta0) + bias0
+        z11 = norm_d1.forward(z11)
         self.a1 = relu_1.relu(z11)
         #print('a1',a1.shape)
     
         z22 = np.dot(self.a1, theta1) + bias1
+        z22 = norm_d2.forward(z22)
         self.a2 = relu_1.relu(z22)
         #print('a2',a2.shape)
     
         z33 = np.dot(self.a2, theta2) + bias2
+        z33 = norm_d3.forward(z33)
         self.a3 = relu_1.relu(z33)
         
         z44 = np.dot(self.a3, theta3) + bias3
@@ -309,17 +361,21 @@ class dense:
         #print('y',y.shape, y)
         dZ4 = ls  
         #print('dZ3',dZ3)
-        dA3 = np.dot(dZ4, theta3.T) * relu_1.relu_derivatived(self.a3)
-        dZ3 = dA3
+        dA3 = np.dot(dZ4, theta3.T) 
+        dA3 = norm_d3.backward(dA3)
+        dZ3 = dA3 * relu_1.relu_derivatived(self.a3)
     
-        dA2 = np.dot(dZ3, theta2.T) * relu_1.relu_derivatived(self.a2)
-        dZ2 = dA2
+        dA2 = np.dot(dZ3, theta2.T) 
+        dA2 = norm_d2.backward(dA2)
+        dZ2 = dA2* relu_1.relu_derivatived(self.a2)
         #print('dZ2',dZ2.shape)
     
-        dA1 = np.dot(dZ2, theta1.T) * relu_1.relu_derivatived(self.a1)
+        dA1 = np.dot(dZ2, theta1.T)
+        dA1 = norm_d1.backward(dA1)
+        dZ1 = dA1 * relu_1.relu_derivatived(self.a1)
         #print('dA1',dA1.shape)
     
-        dA0 = np.dot(dA1, theta0.T) 
+        dA0 = np.dot(dZ1, theta0.T) 
         #print('dA0',dA0.shape)
         
         dtheta3 = np.dot(self.a3.T, dZ4)
@@ -331,42 +387,53 @@ class dense:
         dtheta1 = np.dot(self.a1.T, dZ2)
         dbias1 = np.sum(dZ2, axis=0, keepdims=True)
     
-        dtheta0 = np.dot(x.T, dA1)
-        dbias0 = np.sum(dA1, axis=0, keepdims=True)
+        dtheta0 = np.dot(x.T, dZ1)
+        dbias0 = np.sum(dZ1, axis=0, keepdims=True)
         
         return dtheta0, dbias0, dtheta1, dbias1, dtheta2, dbias2 ,dtheta3 ,dbias3 ,dA0
         
-###############################################################################################################
 
+
+    
+##########################################################################################################
 def conv_forward(batch_im):
     global cache  ,cache2  ,cache5  ,result ,result_2 ,result_3 ,result_4 ,result_5 ,pooling_5   ,pooling_2 ,pooling_1 ,result_relu4 ,result_relu3
     
     result = layer_1.conv2(l1_filter ,batch_im ,b1 ,1)
     result_relu = relu_1.relu(result)
+    r1 = norm1.forward(result_relu)
     
-    pool.update_parms(result_relu , s , ps)
+    pool.update_parms(r1 , s , ps)
     pooling_1 , cache = pool.pooling_max()
-     
+    #print('max 1 out',pooling_1)
+    
     result_2 = layer_1.conv2(l2_filter ,pooling_1 ,b2 ,2)
     result_relu2 = relu_1.relu(result_2)
-    
-    pool.update_parms(result_relu2 , s , ps)
+    r2 = norm2.forward(result_relu2)
+
+    pool.update_parms(r2 , s , ps)
     pooling_2 , cache2 = pool.pooling_max()
+    #print('max 2 out',pooling_2)
 
     result_3 = layer_1.conv2(l3_filter ,pooling_2 ,b3 ,3)
     result_relu3 = relu_1.relu(result_3)
+    r3 = norm3.forward(result_relu3)
+
     
-    
-    result_4 = layer_1.conv2(l4_filter ,result_relu3 ,b4 ,4)
+    result_4 = layer_1.conv2(l4_filter ,r3 ,b4 ,4)
     result_relu4 = relu_1.relu(result_4)
-  
-    
-    result_5 = layer_1.conv2(l5_filter ,result_relu4 ,b5 ,5)
+    r4 = norm4.forward(result_relu4)
+    #print('conv 3 4 out',r4)
+
+    result_5 = layer_1.conv2(l5_filter ,r4 ,b5 ,5)
     result_relu5 = relu_1.relu(result_5)
-    pool.update_parms(result_relu5 , s , ps)
+    r5 = norm5.forward(result_relu5)
+
+    pool.update_parms(r5 , s , ps)
     pooling_5 , cache5  = pool.pooling_max()
-    print('last pooling layer result',pooling_5.shape)
+    
     conv_result , orignal_shape = flat.flatten_forward(pooling_5)
+    #print('conv output',conv_result)
     return conv_result , orignal_shape
 
     
@@ -374,26 +441,29 @@ def conv_forward(batch_im):
 def cnn_backpropogation(dA0, image):
     d1 = flat.flatten_backward(dA0)
     
-    print('reshape     ' , d1.shape)
+    #print('reshape     ' , d1.shape)
     d2 = pool.pooling_backward(d1 , cache5 )
-    d3 = relu_1.relu_derivative(d2 , result_5)
+    
+    r_back5 = norm5.backward(d2)
+    d3 = relu_1.relu_derivative(r_back5 , result_5)
     l5_c ,dw5 ,db5 = layer_1.conv_backward(d3 , l5_filter , result_relu4 , 5) # gradient wrt out ,filter used ,input to layer ,layer no
 
-    d4 = relu_1.relu_derivative(l5_c , result_4)   #gradient wrt out ,input to relu          
+    r_back4 = norm4.backward(l5_c)
+    d4 = relu_1.relu_derivative(r_back4 , result_4)   #gradient wrt out ,input to relu          
     l4_c ,dw4 ,db4 = layer_1.conv_backward(d4 , l4_filter , result_relu3 , 4)  # gradient wrt out ,filter used ,input to layer ,layer no
 
-
-    d5 = relu_1.relu_derivative(l4_c , result_3)    #gradient wrt out ,input to relu      
+    r_back3 = norm3.backward(l4_c)
+    d5 = relu_1.relu_derivative(r_back3 , result_3)    #gradient wrt out ,input to relu      
     l3_c ,dw3 ,db3 = layer_1.conv_backward(d5 , l3_filter , pooling_2 , 3) # gradient wrt out ,filter used ,input to layer ,layer no
 
-    pool.update_parms(l3_c ,s ,ps)
     d6 = pool.pooling_backward(l3_c , cache2 )
-    d7 = relu_1.relu_derivative(d6 ,result_2)           #gradient wrt out ,input to relu      
+    r_back2 = norm2.backward(d6)
+    d7 = relu_1.relu_derivative(r_back2 ,result_2)           #gradient wrt out ,input to relu      
     l2_c ,dw2 ,db2 = layer_1.conv_backward(d7 , l2_filter , pooling_1 , 2) # gradient wrt out ,filter used ,input to layer ,layer no
 
-    pool.update_parms(l2_c ,s ,ps)
     d8 = pool.pooling_backward(l2_c , cache )
-    d9 = relu_1.relu_derivative(d8 ,result)          #gradient wrt out ,input to relu      
+    r_back1 = norm1.backward(d8)
+    d9 = relu_1.relu_derivative(r_back1 ,result)          #gradient wrt out ,input to relu      
     l1_c ,dw1 ,db1 = layer_1.conv_backward(d9 , l1_filter , image , 1) # gradient wrt out ,filter used ,input to layer ,layer no
     #print('input/back conv 1 shape',l1_c.shape)
 
@@ -410,41 +480,56 @@ def categorical_cross_entropy_loss(y_pred, y_true):
     # Return the loss
     return loss
 
+
 #################################################################################################################
 
 def trainf( lr, epochs):
     global theta1, theta2, bias1, bias2, theta0, bias0, theta3, bias3, l1_filter, l2_filter, l3_filter ,l5_filter ,l4_filter , b1, b2, b3 ,b4 ,b5 ,ps ,s
+    global norm1 ,norm2 ,norm3 ,norm4 ,norm5 ,norm_d1 ,norm_d2 ,norm_d3 ,norm_d4
+    global layer_1 , relu_1 , flat , pool , den 
+
     total_loss = 0
     batch_imgsss = batch_img[0]
     batch_lbss = batch_lab[0]
     s = 2
     ps = 3
     print('starting first ')
-    global layer_1 , relu_1 , flat , pool , den
+    
     layer_1 = convolution()
     relu_1 = Activation()
     flat = flatten()
     pool = pooling(batch_imgsss[0] ,s ,ps)
     den = dense()
+
+    norm1 = LayerNormalizationSingleBatch()
+    norm2 = LayerNormalizationSingleBatch()
+    norm3 = LayerNormalizationSingleBatch()
+    norm4 = LayerNormalizationSingleBatch()
+    norm5 = LayerNormalizationSingleBatch()
+
+    norm_d1 = LayerNormalization_dense()
+    norm_d2 = LayerNormalization_dense()
+    norm_d3 = LayerNormalization_dense()
+    norm_d4 = LayerNormalization_dense()
     
     no_l2 = 128       # layer 1
     no_l3 = 64
     no_l4 = 32         
     no_out = 2      # Output layer for 2 classes
     #######################################
-    l1_filter = np.abs( np.random.randn(96 ,11 ,11))
+    l1_filter = ( np.random.randn(96 ,11 ,11))
     b1 = np.zeros((96 ,1 , 1))
     
-    l2_filter = np.abs( np.random.randn(256 ,5 ,5))
+    l2_filter = ( np.random.randn(256 ,5 ,5))
     b2 = np.zeros((256 ,1 , 1))
     
-    l3_filter = np.abs( np.random.randn(384 ,3 ,3))
+    l3_filter = ( np.random.randn(384 ,3 ,3))
     b3 = np.zeros((384 ,1 , 1))
 
-    l4_filter = np.abs( np.random.randn(384 ,3 ,3))
+    l4_filter = ( np.random.randn(384 ,3 ,3))
     b4 = np.zeros((384 ,1 , 1))
     
-    l5_filter = np.abs( np.random.randn(256 ,3 ,3))
+    l5_filter = ( np.random.randn(256 ,3 ,3))
     b5 = np.zeros((256 ,1 , 1))
 
     
@@ -452,11 +537,11 @@ def trainf( lr, epochs):
     #b4=np.zeros((32,1, 1))
     ######################################
     bias0 = np.zeros((1, no_l2))
-    theta1 = np.abs( np.random.rand(no_l2, no_l3))
+    theta1 = ( np.random.rand(no_l2, no_l3))
     bias1 = np.zeros((1, no_l3))
-    theta2 = np.abs( np.random.rand(no_l3, no_l4) )
+    theta2 = ( np.random.rand(no_l3, no_l4) )
     bias2 = np.zeros((1, no_l4))
-    theta3 = np.abs( np.random.rand(no_l4, no_out) )
+    theta3 = ( np.random.rand(no_l4, no_out) )
     bias3 = np.zeros((1, no_out))  
     ########################################
     result ,orignal_shape = conv_forward(batch_imgsss[0])
@@ -525,7 +610,7 @@ def trainf( lr, epochs):
                 batch_dbias2 += dbias2
                 batch_dtheta3 += dtheta3
                 batch_dbias3 += dbias3
-                
+                #print(f"gradient wrt last layer theta {dtheta0}")
                 dw5 ,dw4, dw3 ,dw2 ,dw1 ,db5 ,db4 ,db3 ,db2 ,db1 = cnn_backpropogation(da0 , batch_im[im])
 
                 batch_dw5 += dw5
@@ -567,7 +652,21 @@ def trainf( lr, epochs):
             b1 -= lr * batch_db1 / batch_sz
                 # Printing average loss per epoch
             print(" \n ######################Loss per batch############### =>" ,total_loss/batch_sz)
+            '''
+            print('batch_dtheta3',batch_dtheta3)
+            print('batch_dtheta2',batch_dtheta2)
+            print('batch_dtheta1',batch_dtheta1)
+            print('batch_dtheta0',batch_dtheta0)
 
-lr = 0.001
+            print('batch batch_dw5',batch_dw5)
+            print('batch batch_dw3',batch_dw3)
+            print('batch batch_dw1',batch_dw1)
+
+            print('theta 0',theta0)
+            print('theta 3',theta3)'''
+
+            #print('batch_dtheta0',batch_dtheta0)
+
+lr = 0.0001
 epochs = 10
 trainf(lr, epochs )
